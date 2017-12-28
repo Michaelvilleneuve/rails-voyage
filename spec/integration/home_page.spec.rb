2558864620d3a20c028e70132f7cec17cc0b1b2a
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-12-28 11:15:51 +0100

    Add devise

--- spec/integration/home_page.spec.rb
+++ spec/integration/home_page.spec.rb
@@ -5,4 +5,9 @@ feature 'Homepage' do
     visit '/'
     expect(page).to have_content('Voyages')
   end
+
+  scenario 'can view form' do
+    visit '/'
+    expect(page).to have_css('section.home form')
+  end
 end
